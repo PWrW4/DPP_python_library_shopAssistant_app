acbe0526e18ee127653bea8ea01647ba5cd1d6af
Wojciech Wójcik <wojtek.wroclaw@hotmail.com>
2019-05-16 01:57:57 +0200

    init test

---
+++ test.py
@@ -0,0 +1,10 @@
+from shopAssistant import advice
+from shopAssistant import mealsTypes
+from shopAssistant.yamlOperations import fileOperations
+
+
+mealsTypes.MealType = mealsTypes.MealType.Breakfast
+
+a = advice.Advice()
+
+
